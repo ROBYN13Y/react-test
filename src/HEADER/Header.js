b5094a99ab91a11d055c457f16851fc8e71114cd
ROBYN13Y <yrobyn3@gmail.com>
2022-01-06 15:20:25 -0800

    CREATE component JS
    logo
    .app-container
    .mouse-icon
    .header-text

---
+++ src/HEADER/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Header.css";
+
+import logo from "./mouse.png";
+
+export default function Header() {
+  return (
+    <div className="app-container">
+      <img src={logo} className="mouse-icon" alt="mouse icon" />
+      <p className="header-text">React Testing</p>
+    </div>
+  );
+}
