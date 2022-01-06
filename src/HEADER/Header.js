e6cb3990e7769374261678052e21ffe62f5d715c
ROBYN13Y <yrobyn3@gmail.com>
2022-01-06 15:34:53 -0800

    UPDATE
    .app-header

--- src/HEADER/Header.js
+++ src/HEADER/Header.js
@@ -6,7 +6,7 @@ import logo from "./mouse.png";
 
 export default function Header() {
   return (
-    <div className="app-container">
+    <div className="app-header">
       <img src={logo} className="mouse-icon" alt="mouse icon" />
       <p className="header-text">React Testing</p>
     </div>
