fa69e8977edcaedea0c7eb1f86bc1b81a42972de
ROBYN13Y <yrobyn3@gmail.com>
2022-01-06 15:36:09 -0800

    CREATE component JS
    Footer.css
    .app-footer
    ..footer-text

---
+++ src/FOOTER/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="app-footer">
+      <p className="footer-text">CONNECT</p>
+    </div>
+  );
+}
