5e3df1b2399541f997e65697cbf9875cea6f9f08
ROBYN13Y <yrobyn3@gmail.com>
2022-01-06 17:04:35 -0800

    UPDATE
    ig-icon

--- src/FOOTER/Footer.js
+++ src/FOOTER/Footer.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <div className="app-footer">
-      <p className="footer-text">CONNECT</p>
+      <p className="ig-icon">
+        <FontAwesomeIcon icon={faInstagram} />
+      </p>
+      <p className="footer-text">Coded by Robyn Young</p>
     </div>
   );
 }
