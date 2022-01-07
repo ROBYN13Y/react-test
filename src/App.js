20bcc4bd6d013573fcfe1e41fb1c59e98d346f90
ROBYN13Y <yrobyn3@gmail.com>
2022-01-07 11:29:53 -0800

    UPDATE
    import Test1.js
    h3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from "./HEADER/Header";
 import Footer from "./FOOTER/Footer";
 
+//MAIN SECTION
+import Test1 from "./MAIN/TEST1/Test1";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <h3>TEST1</h3>
+      <Test1 />
       <Footer />
     </div>
   );
