ea0aada441e17290b21939a88305a8a3d2a8f119
ROBYN13Y <yrobyn3@gmail.com>
2022-01-09 12:11:01 -0800

    UPDATE
    BOOTSTRAP
    import Container, Row, Col
    import Avatar, IconButton
    import AddIcon, ForumIcon

--- src/MAIN/TEST1/Test1.js
+++ src/MAIN/TEST1/Test1.js
@@ -2,26 +2,37 @@ import React from "react";
 
 import "./Test1.css";
 
+import { Container, Row, Col } from "react-bootstrap";
+
+import { Avatar, IconButton } from "@mui/material";
+
 //ICONS MATERIAL UI
 //DOCS https://mui.com/components/icons/#main-content
 import SearchIcon from "@mui/icons-material/Search";
-
-import logo from "./g2.png";
+import AddIcon from "@mui/icons-material/Add";
+import ForumIcon from "@mui/icons-material/Forum";
 
 export default function Test1() {
   return (
-    <div className="test1-container">
-      <div className="container-left">
-        <img src={logo} alt="logo icon" className="logo-icon" />
-        <div className="left-text">
+    <Container className="test1-container">
+      <Row>
+        <Col className="col-left">
           <SearchIcon />
-        </div>
-      </div>
-      <div className="container-middle"></div>
-      <div className="container-right">
-        <input type="button" value="BUTTON" />
-        <input type="button" value="BUTTON" />
-      </div>
-    </div>
+          <input type="text" />
+          <div className="left-text"></div>
+        </Col>
+        <Col className="col-middle">
+          <Avatar />
+        </Col>
+        <Col className="col-right">
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+          <IconButton>
+            <ForumIcon />
+          </IconButton>
+        </Col>
+      </Row>
+    </Container>
   );
 }
