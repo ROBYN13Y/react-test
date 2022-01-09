33516d4f3d745e18f2ffd10dd3fb71ffc656e280
ROBYN13Y <yrobyn3@gmail.com>
2022-01-09 13:26:12 -0800

    UPDATE
    trying bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Footer from "./FOOTER/Footer";
 //MAIN SECTION
 import Test1 from "./MAIN/TEST1/Test1";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <h3>TEST1</h3>
-      <Test1 />
-      <Footer />
-    </div>
+    <Container>
+      <Row>
+        <Header />
+      </Row>
+      <Row>
+        <Footer />
+      </Row>
+    </Container>
   );
 }
 
