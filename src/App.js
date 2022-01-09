8b495c61e7e0f3f89c898f04c403070296228a4f
ROBYN13Y <yrobyn3@gmail.com>
2022-01-09 14:29:50 -0800

    UPDATE
    removed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import Footer from "./FOOTER/Footer";
 //MAIN SECTION
 import Test1 from "./MAIN/TEST1/Test1";
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 import "./App.css";
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Header />
-      </Row>
-      <Row>
-        <Footer />
-      </Row>
-    </Container>
+    <div>
+      <Header />
+      <Test1 />
+      <Footer />
+    </div>
   );
 }
 
